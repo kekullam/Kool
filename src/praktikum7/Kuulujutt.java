65bf0c2be49fadcdde790af1b9836693a43abd03
Kerdo Kullamäe <kekullam@klass2-13-l.intra.itcollege.ee>
2015-10-16 11:28:44 +0300

    Pooleli koodid.

--- src/praktikum7/Kuulujutt.java
+++ src/praktikum7/Kuulujutt.java
@@ -1,9 +1,9 @@
 package praktikum7;
 
-public class reverseNumbers {
+public class Kuulujutt {
 
 	public static void main(String[] args) {
-		Tegin selle muudatuse kodus
+		// TODO Auto-generated method stub
 
 	}
 
---
+++ src/praktikum7/ReverseNumbers.java
@@ -0,0 +1,23 @@
+package praktikum7;
+
+import lib.TextIO;
+
+public class ReverseNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number to reverse: ");
+		int num = TextIO.getInt();
+		int reverse = 0;
+		while (num != 0) {
+			reverse = reverse * 10;
+			reverse = reverse + num%10;
+			num = num/10;
+		
+			
+			
+		}
+		System.out.println(reverse);
+
+	}
+
+}
