faeceb69767c1eaa2367e4f4dc8d2d27f4971a81
Kerdo Kullamäe <kekullam@klass2-08-l.intra.itcollege.ee>
2015-10-30 11:28:49 +0200

    praktikum9 pooleli :x

--- src/praktikum9/TulpDiagramm.java
+++ src/praktikum9/TulpDiagramm.java
@@ -6,12 +6,37 @@ import java.util.Scanner;
 public class TulpDiagramm {
 
 	public static Scanner userInput = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		
-		ArrayList<Integer[]> numbrid = new ArrayList<Integer[]>();
-		int _userInput = userInput.nextLine();
-		
+
+		ArrayList<Integer> numbrid = new ArrayList<Integer>();
+
+		int number = 1;
+		while (number > 0) {
+			System.out.println("Sisesta number: ");
+			number = getNumber();
+			numbrid.add(number);
+		}
+		if (number == 0) {
+			System.out.println(numbrid + arvX(numbrid));
+
+		}
+		userInput.close();
+	}
+
+	public static String arvX(int a) {
+		String x = "";
+		for (int i = 0; i < a; i++) {
+			x += "x";
+		}
+		return x;
+
+	}
+
+	private static int getNumber() {
+		int number = 0;
+		number = userInput.nextInt();
+		return number;
 	}
 
 }
