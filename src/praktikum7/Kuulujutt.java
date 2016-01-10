54d0f755c20d78b9657f6aec4295b78c6129d593
kekullam <kerdo.kullamae@gmail.com>
2016-01-10 22:55:02 +0200

    practise for exam

--- src/praktikum7/Kuulujutt.java
+++ src/praktikum7/Kuulujutt.java
@@ -2,9 +2,21 @@ package praktikum7;
 
 public class Kuulujutt {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		public static void main(String[] args){
+			String[] nimed1 = {"Kati", "Tiina", "Liisu", "Nele"};
+			String[] tegus6nad = {"Paneb", "Leiab", "Näeb", "Teab"};
+			String[] nimed2 = {"T6nu", "Martin", "Margus"};
+
+			System.out.println(nimed1[ranGen(nimed1.length)] + " " +
+					tegus6nad[ranGen(tegus6nad.length)] + " " +
+					nimed2[ranGen(nimed2.length)]);
+		}
+
+		private static int ranGen(int _length){
+			return (int) (Math.random() * _length);
+		}
 
 	}
 
-}
+
